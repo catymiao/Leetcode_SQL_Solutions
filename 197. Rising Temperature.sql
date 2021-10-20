a934992a5dff695480347f4b99d13ee56fcddcc8
catymiao <33770231+catymiao@users.noreply.github.com>
2021-10-20 15:38:17 -0500

    Add files via upload

--- 197. Rising Temperature.sql
+++ 197. Rising Temperature.sql
@@ -1,11 +1,11 @@
 # Write your MySQL query statement below
 SELECT  b.Id AS "Id" from Weather a
 Left join weather b 
-ON DATEDIFF(b.recordDate, a.recordDate) = 1
+ON DATEDIFF(b.recordDate, a.recordDate) = 1 # First date - Second date
 WHERE a.temperature < b.temperature;
 
 /* Write your T-SQL query statement below */
 SELECT w2.Id from Weather w1
 LEFT JOIN Weather w2
-ON DATEDIFF(day, w1.RecordDate, w2.RecordDate)=1
+ON DATEDIFF(day, w1.RecordDate, w2.RecordDate) = 1 /* Second date - First date
 WHERE w1.Temperature < w2.Temperature 
