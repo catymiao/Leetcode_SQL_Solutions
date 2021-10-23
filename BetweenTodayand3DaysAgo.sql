7ae338bd2be344996ef70f3b082219344d14a27b
catymiao <33770231+catymiao@users.noreply.github.com>
2021-10-23 10:37:38 -0500

    Update BetweenTodayand3DaysAgo.sql

--- BetweenTodayand3DaysAgo.sql
+++ BetweenTodayand3DaysAgo.sql
@@ -5,4 +5,4 @@ select * from employee where date between DATEADD(DAY, -3, @today) and @today
 
 # method 2 
 select * from employee 
-where date between DATEADD(day, -3, CAST(GETDATE() AS date)) and convert(date, getdate())
+where date between DATEADD(day, -3, CAST(GETDATE() AS date)) and convert(date, GETDATE())
