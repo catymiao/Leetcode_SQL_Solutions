ff33f5bfe6f36c60a160e688cd517950901c3d62
catymiao <33770231+catymiao@users.noreply.github.com>
2021-10-26 11:15:37 -0500

    Delete BetweenTodayand3DaysAgo.sql

--- BetweenTodayand3DaysAgo.sql
+++
@@ -1,8 +0,0 @@
-# method 1
-DECLARE @today date = GETDATE();
-select * from employee where date between DATEADD(DAY, -3, @today) and @today
-
-
-# method 2 
-select * from employee 
-where date between DATEADD(day, -3, CAST(GETDATE() AS date)) and convert(date, GETDATE())
