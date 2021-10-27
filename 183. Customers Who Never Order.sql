a25d7af426266bb853c547c5c149a206d2edceda
catymiao <33770231+catymiao@users.noreply.github.com>
2021-10-26 21:07:00 -0500

    Update 183. Customers Who Never Order.sql

--- 183. Customers Who Never Order.sql
+++ 183. Customers Who Never Order.sql
@@ -1,6 +1,7 @@
+/* Write your T-SQL query statement below */
 # Write your MySQL query statement below
 
 SELECT  NAME AS Customers FROM CUSTOMERS c
 LEFT JOIN Orders o 
 ON c.Id = o.CustomerId
-where CustomerId IS NULL
+where CustomerId IS NULL;
