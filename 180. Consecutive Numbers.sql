350e63c9c42841d05f2c35d806d43a8cd389a988
catymiao <33770231+catymiao@users.noreply.github.com>
2021-10-26 20:56:37 -0500

    Update 180. Consecutive Numbers.sql

--- 180. Consecutive Numbers.sql
+++ 180. Consecutive Numbers.sql
@@ -1,4 +1,6 @@
-# Write your MySQL query statement below
+# Write an SQL query to find all numbers that appear at least three times consecutively.
+
+# Write your MySQL/T-SQL query statement below
 SELECT distinct l1.Num AS ConsecutiveNums 
 from Logs l1
 inner join logs l2 
@@ -9,3 +11,10 @@ where l1.num = l2.num
 and l2.num = l3.num
 
 # 373 ms, faster than 98.21% of MySQL online submissions for Consecutive Numbers.
+
+/* Write your MySQL/T-SQL query statement below*/
+Select distinct l1.Num as consecutivenums from Logs l1, logs l2, logs l3 
+Where l1.num = l2.num 
+And L2.num = l3.num 
+And l1.id = l2.id+1
+And l2.id = l3.id+1; 
