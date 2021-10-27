a8dfe27096e62379ab4807b6211ac4d9f85440b1
catymiao <33770231+catymiao@users.noreply.github.com>
2021-10-26 21:00:23 -0500

    Update 181. Employees Earning More Than Their Managers.sql

--- 181. Employees Earning More Than Their Managers.sql
+++ 181. Employees Earning More Than Their Managers.sql
@@ -1,6 +1,6 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
 select Employee from (
-	select e.id, e.Name Employee ,e.Salary EmployeeSal, m.Salary ManagerSal from employee e
+	select e.Name as Employee ,e.Salary EmployeeSal, m.Salary ManagerSal from employee e
     inner join employee m 
     on e.managerID = m.Id)a
 where EmployeeSal > ManagerSal
