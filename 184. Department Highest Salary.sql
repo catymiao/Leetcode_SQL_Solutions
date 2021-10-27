39f9dd2eeee38d4430a6cfeaa8599b9f517b7aa1
catymiao <33770231+catymiao@users.noreply.github.com>
2021-10-26 21:25:27 -0500

    Update 184. Department Highest Salary.sql

--- 184. Department Highest Salary.sql
+++ 184. Department Highest Salary.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+# Write your MySQL/T-SQL query statement below
 
 SELECT d.Name AS Department, Employee , SALARY From (  
 select a.departmentId, a.Salary, e.Name Employee from (
@@ -12,3 +12,13 @@ LEFT JOIN Department d
 on b.DepartmentId = d.Id 
 
 # Runtime: 475 ms, faster than 97.15% of MySQL online submissions for Department Highest Salary.
+
+
+/* Write your T-SQL query statement below */
+SELECT department, employee, salary FROM (
+SELECT department, employee, salary, rank() over (partition by department order by Salary desc) AS Rank FROM( 
+SELECT e.name as employee, salary, d.name as department
+FROM Employee e
+LEFT JOIN Department d
+on e.departmentId = d.id )a)b 
+where rank = 1 
