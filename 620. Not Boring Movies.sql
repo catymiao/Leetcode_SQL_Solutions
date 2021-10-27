5f85c73079149e1d89313863ab3b4651060cba08
catymiao <33770231+catymiao@users.noreply.github.com>
2021-10-26 22:34:19 -0500

    Update 620. Not Boring Movies.sql

--- 620. Not Boring Movies.sql
+++ 620. Not Boring Movies.sql
@@ -4,3 +4,9 @@ SELECT * FROM Cinema
 where description <> 'boring'
 and mod(id,2) <> 0 
 order by rating DESC
+
+/* Write your T-SQL query statement below */
+SELECT * FROM Cinema
+where description <> 'boring'
+and id%2 <> 0 
+order by rating DESC
