88ea0ec4a8b4074ca1a28aa9772e72b63d2ff55c
catymiao <33770231+catymiao@users.noreply.github.com>
2021-10-26 21:29:40 -0500

    Update 185. Department Top Three Salaries(TBR).sql

--- 185. Department Top Three Salaries(TBR).sql
+++ 185. Department Top Three Salaries(TBR).sql
@@ -1,5 +1,4 @@
 /* Write your T-SQL query statement below */
-/* My SQL only */
 
 SELECT Department.name AS Department, Employee, Salary FROM (
 SELECT Employee, Salary, DepartmentId FROM (
@@ -9,3 +8,12 @@ SELECT Employee.Name AS Employee, Salary, DepartmentId, Dense_Rank() OVER
  WHERE Rank<=3)b
  INNER JOIN Department 
  ON b.DepartmentId = Department.Id
+ 
+ 
+ /* Write your T-SQL query statement below */
+Select Department, Employee, Salary from (
+Select d.Name AS Department, e.name AS Employee, e.Salary, 
+dense_rank() over (partition by d.Name order by Salary desc) as rank            
+FROM Department d, Employee e
+WHERE e.DepartmentId = d.Id)a 
+WHERE rank between 1 and 3 ;
