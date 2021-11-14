997e50ee61a986f7bfad922bcd1ee555cdde2f4d
catymiao <33770231+catymiao@users.noreply.github.com>
2021-11-14 16:58:10 -0600

    Update and rename 185. Department Top Three Salaries(TBR).sql to 185. Department Top Three Salaries.sql

--- 185. Department Top Three Salaries.sql
+++ 185. Department Top Three Salaries.sql
@@ -17,3 +17,11 @@ dense_rank() over (partition by d.Name order by Salary desc) as rank
 FROM Department d, Employee e
 WHERE e.DepartmentId = d.Id)a 
 WHERE rank between 1 and 3 ;
+
+
+ /* Write your T-SQL query statement below */
+select Department, Employee, Salary from (
+SELECT d.name as Department, e.name as Employee, Salary , dense_rank() OVER (partition by DepartmentId order by salary desc) as rnk
+FROM Employee e,  Department d 
+WHERE departmentId  = d.id) a 
+where rnk <=3 
